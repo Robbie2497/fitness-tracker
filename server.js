190d488937df82405eca110a5072f689b687fb16
Robert McGrew <robbie2497@gmail.com>
2020-08-17 21:40:36 -0600

    removing get route

--- server.js
+++ server.js
@@ -17,11 +17,6 @@ mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
-app.get("/", function(req, res) {
-  res.json(path.join(__dirname, "public/index.html"));
-});
-
 // routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
